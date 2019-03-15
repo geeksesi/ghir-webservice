cbf538c54c740350e5d1a3cb1012396e21077c49
mohammad javad ghasemy <geeksesi@gmail.com>
2019-03-15 04:17:06 +0330

    basic of request
    get|post|update
    make basis of router

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,21 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return "hello world";
-});
+$router->get('/', 'Home');
 
 $router->get('/get/sell', 'Get@sell');
+$router->get('/get/buy', 'Get@buy');
+$router->get('/get/user', 'Get@user');
+$router->get('/get/product', 'Get@product');
+
+$router->get('/set/sell', 'Set@sell');
+$router->get('/set/buy', 'Set@buy');
+$router->get('/set/user', 'Set@user');
+$router->get('/set/product', 'Set@product');
+$router->get('/set/product_price', 'Set@product_price');
+
+$router->get('/update/sell', 'Update@sell');
+$router->get('/update/buy', 'Update@buy');
+$router->get('/update/user', 'Update@user');
+$router->get('/update/product', 'Update@product');
+$router->get('/update/product_price', 'Update@product_price');
