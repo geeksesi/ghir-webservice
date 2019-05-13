a0a04c09f1337a96eb0f8733125338072a72e3be
mohammad javad ghasemy <geeksesi@gmail.com>
2019-05-13 12:30:06 +0430

    finish user & v1 routing & delete useless documents

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -11,30 +11,7 @@ class User extends Controller
 {
 
 
-    public function get(Request $req)
-    {
-        $response = [];
-        $status = ["status"=>"ok"];
-        if (!$req->has('status'))
-        {
-           return "please enter status";
-        }
-        $data = "hi";
-        
-        if ($data === false)
-        {
-            $response["status"]= "ok";
-            return response()->json($response);
-        }
-
-        $response["status"] = "ok";
-        $response["data"] = $data;
-
-        return response()->json($response);
-
-    }
-
-    public function auth(Request $req)
+    public function login(Request $req)
     {
         $response = [];
         if(!$req->has('hash'))
@@ -52,53 +29,64 @@ class User extends Controller
          * @var [type]
          */
         $auth_array = $this->un_hash($req->input('hash'));
-        $user_info = $this->auth_db($auth_array["user_name"]);
+        $user_info  = $this->auth_db($auth_array["user_name"]);
 
         if(!isset($user_info->all('password')[0]))
         {
             $response["status"] = "false";
             $response["message"] = "wrong user_name";
-    
+            
             return response()->json($response);    
         }
-        if(password_verify($auth_array["password"], $user_info->all('password')[0]->password) && $auth_array["phone_number"] != $user_info->all('phone_number')[0]->phone_number)
+        $user_id      = $user_info->all()[0]->id;
+        $password     = $user_info->all('password')[0]->password;
+        $phone_number = $user_info->all('phone_number')[0]->phone_number;
+        if(!password_verify($auth_array["password"], $password))
         {
-            $response["status"] = "false";
-            $response["message"] = "wrong phone_number or password";
+
+            $response["status"]  = "false";
+            $response["message"] = "wrong password";
+    
+            return response()->json($response);
+        }
+        
+        if($phone_number === null)
+        {
+            $this->set_phone($user_id, $auth_array["phone_number"]);
+        }
+        elseif($auth_array["phone_number"] != $phone_number)
+        {
+            $response["status"]  = "false";
+            $response["message"] = "wrong phone_number";
     
             return response()->json($response);
         }
 
         $response["status"] = "ok";
-        $response["data"] = $data;
+        $response["data"]   = ["token" => $this->make_token($user_id)];
 
         return response()->json($response);
     }
  
 
+    public function set_phone($_user_id, $_phone_number)
+    {
+        $db_result = app('db')->table('user')
+            ->where('id', $_user_id)
+            ->update(["phone_number" => $_phone_number]);
+        
+        return $db_result;  
+    }
+    
     public function auth_db($_user_name)
     {
         $db_result = app('db')->table('user')
             ->select('id', 'user_name', 'password', 'phone_number')
             ->where('user_name', $_user_name)
             ->get();
+
         return $db_result;        
     }
  
-    public function set(Request $req)
-    {
-        $en = Crypt::encrypt("hello_world");
-        var_dump($en);
-        var_dump(Crypt::decrypt($en));
-        return "hello Set/sell";
-    }
- 
- 
- 
-    public function update(Request $req)
-    {
-        return "hello Set/sell";
-    }
-
 
 }
--- routes/web.php
+++ routes/web.php
@@ -13,46 +13,18 @@
 // Home
 $router->get('/', 'Home@home');
 
-// Get
-// Buy
-$router->get('/get/buy',                'Buy@get');
-$router->get('/get/buy/id',             'Buy@id');
-$router->get('/get/buy/user',           'Buy@user');
-$router->get('/get/buy/timestamp',      'Buy@timestamp');
+/**
+ * simple just for low level users ( means non admin users)
+ * 
+ * they just need :
+ * - login
+ * - set order
+ * - show self order's
+ * - show self position's  
+ */
 
-// Sell
-$router->get('/get/sell',                'Sell@get');
-$router->get('/get/sell/id',             'Sell@id');
-$router->get('/get/sell/user',           'Sell@user');
-$router->get('/get/sell/timestamp',      'Sell@timestamp');
-
-// Position 
-$router->get('/get/position',           'Position@get');
-$router->get('/get/position/id',        'Position@id');
-$router->get('/get/position/type',        'Position@type');
-$router->get('/get/position/user',      'Position@user');
-$router->get('/get/position/timestamp', 'Position@timestamp');
-
-
-// User
-$router->get('/get/user',  'User@get');
-$router->post('/get/user/auth',  'User@auth');
-
-// Account
-$router->get('/get/account', 'Account@get');
-
-
-
-// Set
-$router->post('/set/buy', 'Buy@set');
-$router->post('/set/sell', 'Sell@set');
-$router->post('/set/user', 'User@set');
-$router->post('/set/position', 'Position@set');
-$router->post('/set/account', 'Account@set');
-
-// Update
-$router->post('/update/sell', 'Sell@update');
-$router->post('/update/buy', 'Buy@update');
-$router->post('/update/user', 'User@update');
-$router->post('/update/position', 'Position@update');
-$router->post('/update/account', 'Account@update');
+$router->post('/login', 'User@login');
+$router->post('/{token}/order', 'Order@set');
+$router->get('/{token}/order', 'Order@get');
+$router->get('/{token}/order', 'Order@get');
+$router->get('/{token}/position', 'Position@get');
